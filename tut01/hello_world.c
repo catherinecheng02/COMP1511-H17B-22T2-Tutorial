c53a5ee50db7066a5377944b8b768eee9cbbdd61
catherinecheng02 <z5310517@ad.unsw.edu.au>
2022-06-02 18:21:38 +1000

    Create hello_world.c

---
+++ tut01/hello_world.c
@@ -0,0 +1,14 @@
+// Basic Hello World program
+// Marc Chee, September 2020
+
+#include <stdio.h> // including a standard library 
+
+int main (void) { // main function, doesn't take in any arguments, return value is int 
+    printf("Hello World\n"); // prints "Hello World" and an enter on terminal
+    // \n means enter or newline
+   
+    return 0; // returning control to computer
+}
+
+// indentation - code style, makes code neater. generally, indent by 4 spaces after
+// every curly bracket
